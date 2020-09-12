043ffa82fd4fecc615833d6f18c437f09154d49d
gb85150 <geoffrey.bousseau@gmail.com>
2020-09-13 00:47:07 +0200

    correction/lisibilité 13/09/2020 00:47

--- Date.py
+++ Date.py
@@ -1,6 +1,8 @@
 an = int(input("l'année"))
 mois = int(input("le mois au format 0X ou XX"))
 # vérification année bissextile
+
+
 def bissextile(an):
     if (an % 4) == 0 or (an % 400) == 0:
         isbissextile = True
@@ -13,24 +15,32 @@ def bissextile(an):
     return(isbissextile)
 # print(bissextile(an))
 # calcul nb de jours pour 1an
+
+
 def nbjoursannee(isbissextile):
     nban = 0
-    if isbissextile == True:
+    if isbissextile is True:
         nban = 366
     else:
         nban = 365
     return(nban)
+
+
 print(nban)
 # nbjour pour 1mois
+
+
 def nbjoursmois(isbissextile, mois):
     if mois == 1 or mois == 3 or mois == 5 or mois == 7 or mois == 8 or mois == 10 or mois == 12:
         nbmois = 31
     elif mois == 2:
-        if isbissextile == True:
+        if isbissextile is True:
             nbmois = 29
         else:
             nbmois = 28
     else:
         nbmois = 30
     return(nbmois)
+
+
 print(nbmois)
