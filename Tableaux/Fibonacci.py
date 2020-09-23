85b480129caf4f7c657370c21a7ce29e39eb80c0
gb85150 <geoffrey.bousseau@gmail.com>
2020-09-23 23:10:51 +0200

    correction de range(a,b) vers range(a,b-1)

--- Tableaux/Fibonacci.py
+++ Tableaux/Fibonacci.py
@@ -1,6 +1,6 @@
 # Fibonacci
 suite_fibonacci = [0, 1]
-for i in range(2, 30):
+for i in range(2, 31):
     suite_fibonacci.append(suite_fibonacci[i-1]+suite_fibonacci[i-2])
 if suite_fibonacci[30] == 514229:
     print(suite_fibonacci)
