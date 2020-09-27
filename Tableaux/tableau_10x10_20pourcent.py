bdc5d585289915a9355064c2e6289ff1ab5d6426
gb85150 <geoffrey.bousseau@gmail.com>
2020-09-27 18:37:03 +0200

    mise en forme du script

--- Tableaux/tableau_10x10_20pourcent.py
+++ Tableaux/tableau_10x10_20pourcent.py
@@ -1,28 +1,28 @@
-#tableau random
+# tableau random
 import random
 
 
 def listgen():
-    probabilité = (0,0,0,0,1)
+    probabilité = (0, 0, 0, 0, 1)
     liste = [[random.choice(probabilité) for i in range(10)] for i in range(10)]
     return liste
+
 
 def guessuser():
     x = int(input("sélectionnez la position de x (va de 1 à 10)"))
     y = int(input("sélectionnez la position de y (va de 1 à 10)"))
-    guess = (x,y)
+    guess = (x, y)
     return guess
-test 
 
 
-tab=listgen()
+tab = listgen()
 nbde1 = 0
 for i in range(10):
     nbde1 = nbde1 + tab[i].count(1)
 print(nbde1)
 for i in (range(3)):
     print("tentative n°{} : ".format(i))
+# gessnum avec le numéro de la tentative
     guessnum{}.format(i) = guessuser()
 # for i in range(10):
 #     print(tab[i])
-    
