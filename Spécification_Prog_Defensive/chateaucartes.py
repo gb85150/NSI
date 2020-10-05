0a12d56d06dd69c0e40fb47d184439acbf352488
gb85150 <geoffrey.bousseau@gmail.com>
2020-10-05 09:13:33 +0200

    1er test de la fonction numéro 2

--- Spécification_Prog_Defensive/chateaucartes.py
+++ Spécification_Prog_Defensive/chateaucartes.py
@@ -6,11 +6,13 @@ def nbr_cartes(etages: int) -> int:
     :result: nbre de cartes nécessaires à la construction du chateau de cartes
     de :etages:
     """
+    assert etages >= 1, le nombre d'étages doit être un entier positif
     somme = 0
-    for i in range(1;étages):
+    for i in range(1;etages):
         somme = somme + i * 3 - 1
+        numetage = i
     assert somme > 1, Erreur de calcul de somme
-    return somme
+    return somme, numetage
 
 
 def testnbre(nbrecartes: int) -> int:
@@ -21,4 +23,6 @@ def testnbre(nbrecartes: int) -> int:
     :param nbrecartes: nbre de cartes disponibles
     :result: nbre d'étages pouvant être contruits
     """
-    for étages in range()
+    somme = 0
+    while somme <= nbrecartes:
+        somme = nbr_cartes(etages)
