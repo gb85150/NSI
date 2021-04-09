38d9bb5d958430cf4aac4110536cd4910c56a626
gb85150 <geoffrey.bousseau@gmail.com>
2021-04-09 17:12:02 +0200

    edit 09/04/2021 16:30

--- tkter/Exercice1.py
+++ tkter/Exercice1.py
@@ -1,12 +1,12 @@
-from tkinter import *
+import tkinter as tk
 
 
-fen1 = Tk()
+fen1 = tk.Tk()
 fen1.title("pranked")
 fen1.config(bg="Red")
 fen1.geometry("400x100")
-Ok = Button(fen1, text='Ok !', command=fen1.destroy)
-text = Label(fen1, fg='Red', text="""Bonjour tout le monde
+Ok = tk.Button(fen1, text='Ok !', command=fen1.destroy)
+text = tk.Label(fen1, fg='Red', text="""Bonjour tout le monde
 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 Mauris vel aliquet augue, ac accumsan augue. Nam eleifend,
 enim et egestas posuere, nulla ipsum suscipit erat.
--- tkter/exercice1_1prenom.py
+++ tkter/exercice1_1prenom.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 # FONCTIONS
 
 
@@ -14,34 +14,34 @@ def afficher(event):
     """
     champ_label2.configure(text="bonjour " + texte.get())
     return None
-    # PROGRAMME PRINCIPAL
-    fen1 = Tk()
-    # On dimensionne la fenetre
-    fen1.geometry("400x100")
-    # On lui donne un titre
-    fen1.title("Ma fenêtre")
-    # On la colore
-    fen1.config(background="Yellow")
-    # On crée un label (ligne de texte) de titre
-    champ_label = Label(fen1, text="Bienvenue dans ce nouveau monde !")
-    # On affiche le label dans la fenêtre, au milieu par défaut
-    champ_label.pack()
-    # On crée un deuxième label
-    champ_label2 = Label(fen1, text="Entrez votre nom")
-    # On affiche le label dans la fenêtre, à gauche
-    champ_label2.pack(side=LEFT)
-    # On crée puis un champ de saisie
-    # le champ de saisie donne une variable nommée texte
-    texte = Entry(fen1, width=20)
-    # quand on appuie sur entrée, on appelle la fonction "afficher"
-    texte.bind("<Return>", afficher)
-    # On affiche le texte dans la fenêtre
 
 
+# PROGRAMME PRINCIPAL
+fen1 = tk.Tk()
+# On dimensionne la fenetre
+fen1.geometry("400x100")
+# On lui donne un titre
+fen1.title("Ma fenêtre")
+# On la colore
+fen1.config(background="Yellow")
+# On crée un label (ligne de texte) de titre
+champ_label = tk.Label(fen1, text="Bienvenue dans ce nouveau monde !")
+# On affiche le label dans la fenêtre, au milieu par défaut
+champ_label.pack()
+# On crée un deuxième label
+champ_label2 = tk.Label(fen1, text="Entrez votre nom")
+# On affiche le label dans la fenêtre, à gauche
+champ_label2.pack(side="left")
+# On crée puis un champ de saisie
+# le champ de saisie donne une variable nommée texte
+texte = tk.Entry(fen1, width=20)
+# quand on appuie sur entrée, on appelle la fonction "afficher"
+texte.bind("<Return>", afficher)
+# On affiche le texte dans la fenêtre
 texte.pack()
 # on ajoute un bouton "quitter", avec une commande fen1.destroy au nom
 
-bouton_quitter = Button(fen1, text="Quitter", command=fen1.destroy)
+bouton_quitter = tk.Button(fen1, text="Quitter", command=fen1.destroy)
 # On affiche le bouton dans la fenêtre
 bouton_quitter.pack()
 # On démarre la boucle Tkinter qui s'interrompt quand on ferme la fenêtre
