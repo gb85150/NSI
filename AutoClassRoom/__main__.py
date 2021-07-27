389373060cc384771c7c8a08a51eb1c3cdce1173
gb85150 <geoffrey.bousseau@gmail.com>
2021-07-28 01:43:01 +0200

    Specifications and new print system

--- AutoClassRoom/__main__.py
+++ AutoClassRoom/__main__.py
@@ -90,17 +90,18 @@ Merci d'indiquer ces codes pour chaque question
 
 
 # Bug
-def loadcsv(file):
+def loadcsv(file: str) -> list:
     with open(file, "r") as fichier:
         rawdata = csv.reader(fichier, delimiter=';')
         output = list
         for i in rawdata:
-            output.append(i)
+            print(i)
+            output.append(list(i))
     return output
 
 
 # Fini (pour l'instant)
-def getinfo():
+def getinfo() -> tuple:
     spe1 = input("Spécialité 1 : ")
     spe2 = input("Spécialité 2 : ")
     lang = input("Langue Additionnelle : ")
@@ -110,30 +111,49 @@ def getinfo():
     return spe1, spe2, lang
 
 
-def findclass(classe, spe1, spe2, lang, classeeleve=None):
-    for i in classe:
+# Fini/Non testé
+def findclass(classe: list, spe1: str, spe2: str, lang: str, classeeleve=None) -> int:
+    for i in range(classe):
         if spe1 in classe[i] and spe2 in classe[i] and lang in classe[i]:
             classeeleve = i
             return classeeleve
         else:
-            print("Fetching Failed ! Please make sure you gave the right answers")
+            exit("findclass : Fetching Failed ! Please make sure you gave the right answers")
             break
 
 
+# Fini/Non testé
 def fetchclassletter(classeeleve):
     listletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
     lettreclasse = listletters[classeeleve]
     return lettreclasse.upper()
 
 
+def newprint(speech: int):
+    content = loadcsv("speech.csv")
+    '''
+    # dev only :
+    content = ["=====================================================================================================================\n=====================================================================================================================\n===       ___           ___           ___           ___           ___           ___       ___           ___       ===\n===      /\  \         /\__\         /\  \         /\  \         /\  \         /\__\     /\  \         /\  \      ===\n===     /::\  \       /:/  /         \:\  \       /::\  \       /::\  \       /:/  /    /::\  \       /::\  \     ===\n===    /:/\:\  \     /:/  /           \:\  \     /:/\:\  \     /:/\:\  \     /:/  /    /:/\:\  \     /:/\ \  \    ===\n===   /::\~\:\  \   /:/  /  ___       /::\  \   /:/  \:\  \   /:/  \:\  \   /:/  /    /::\~\:\  \   _\:\~\ \  \   ===\n===  /:/\:\ \:\__\ /:/__/  /\__\     /:/\:\__\ /:/__/ \:\__\ /:/__/ \:\__\ /:/__/    /:/\:\ \:\__\ /\ \:\ \ \__\  ===\n===  \/__\:\/:/  / \:\  \ /:/  /    /:/  \/__/ \:\  \ /:/  / \:\  \  \/__/ \:\  \    \/__\:\/:/  / \:\ \:\ \/__/  ===\n===       \::/  /   \:\  /:/  /    /:/  /       \:\  /:/  /   \:\  \        \:\  \        \::/  /   \:\ \:\__\    ===\n===       /:/  /     \:\/:/  /     \/__/         \:\/:/  /     \:\  \        \:\  \       /:/  /     \:\/:/  /    ===\n===      /:/  /       \::/  /                     \::/  /       \:\__\        \:\__\     /:/  /       \::/  /     ===\n===      \/__/         \/__/                       \/__/         \/__/         \/__/     \/__/         \/__/      ===\n===                                                                                                               ===\n===                                              CLASSROOM RESOLVER                                               ===\n=====================================================================================================================\n=====================================================================================================================",
+               "CODE Spés :\n\n - NSI      = Numérique et Sciences de l'Informatique\n - HGGSP    = Histoire Géographie Géopolitique et Sciences Politiques\n - HLP      =\n - SI       = Sciences de l'Ingénieur\n - SES      = Sciences économiques et sociales\n - MATHS    = Mathématiques\n - LLCE_ENG = Anglais\n - LLCE_ESP = Espagnol\n - PC       = Physique Chimie\n - SVT      = Sciences de la Vie et de la Terre\n - ART_P    = Arts Plastiques\n - MUSIC    = Musique\n - THEATRE  = Théâtre",
+               "CODE Langues :\nEtant donné qu'une erreur a probablement été effectuée lors des réglages des groupes de langues,\naucune différence sera faite entre LV1 et LV2.\n\n - ALL = Allemand\n - ESP = Espagnol\n - CHI = Chinois",
+               "CODE Options (Faculatif/Non implémenté) :\n - MATHSEXPERT\n - MATHSCOMP\n - DGEMC\n - EUROENG\n - EUROALL\n - ART_P\n - MUSIC\n - THEATRE\n - CHI3\n - LATIN\n - GREC"]
+    '''
+    if speech >= 1:
+        clearconsole()
+        print(content[speech])
+    else:
+        clearconsole()
+        print("Merci de répondre avec le code correspondant")
+        print(content[speech])
+    return None
+
+
 # printinfo()
 # CLASSE = loadcsv("classesptvirgule.csv")
-print(loadcsv("speech.csv"))
+print(loadcsv("classesptvirgule.csv"))
 # print(CLASSE)
+# newprint(3)
+
 # INFO = getinfo()
 # CLASSEELEVE = findclass(CLASSE, INFO[0], INFO[1], INFO[2])
 # print(fetchclassletter(CLASSEELEVE))
-
-# print(SPE1)
-# print(SPE2)
-# print(LANG)
