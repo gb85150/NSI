891086adbeb443a07d1d2ab6db18487f6f290c4f
sf3868 <geoffrey.bousseau@gmail.com>
2021-09-10 14:32:31 +0200

    Ajout de mon fichier

--- Domino/Domino.py
+++ Domino/Domino.py
@@ -1,4 +1,8 @@
+import Geoffrey
 # Main project
 class Dominos:
+    """The dominos game is composed of tiles painted with two numbers from 0 to 6
+    """
     def __init__(self):
+        generate_dominos()
         pass
