9a8e4e17cd26e50d80bc14ebe7526989dc0c5b38
sf3868 <geoffrey.bousseau@gmail.com>
2021-09-10 14:57:56 +0200

    14h57

--- Domino/Domino.py
+++ Domino/Domino.py
@@ -1,11 +1,20 @@
 from Geoffrey import generate
 import random
 # Main project
+
+
 class Dominos:
     """The dominos game is composed of tiles painted with two numbers from 0 to 6
+    Two sides :
+    - A from 0 to 6
+    - B from 0 to 6
     """
     def __init__(self):
-        self.A = random(0, 7)
+        self.__A = list
+        self.__B = list
+        self.__A += random.randint(0, 7)
+        self.__B += random.randint(0, 7)
         pass
 
-deck = generate()
+
+deck = generate(Dominos)
--- Domino/Geoffrey.py
+++ Domino/Geoffrey.py
@@ -4,3 +4,11 @@ def generate(dominos):
     for i in range(28):
         jeu_dominos += dominos
     return jeu_dominos
+
+
+def test_recurrence(dominos):
+    double = list
+    for i in range(28):
+        if dominos.__A[i] == dominos.__B[i]:
+            double += i
+    return double
