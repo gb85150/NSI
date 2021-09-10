0ab7a6267582d4a31049ba916aa33c591acc7565
sf3868 <geoffrey.bousseau@gmail.com>
2021-09-10 14:43:58 +0200

    Etablissement du lien etre les différents fichiers

--- Domino/Domino.py
+++ Domino/Domino.py
@@ -1,8 +1,11 @@
-import Geoffrey
+from Geoffrey import generate
+import random
 # Main project
 class Dominos:
     """The dominos game is composed of tiles painted with two numbers from 0 to 6
     """
     def __init__(self):
-        generate_dominos()
+        self.A = random(0, 7)
         pass
+
+deck = generate()
--- Domino/Geoffrey.py
+++ Domino/Geoffrey.py
@@ -1,2 +1,6 @@
-def Generate():
+# Functions de geoffrey
+def generate(dominos):
+    jeu_dominos = list
     for i in range(28):
+        jeu_dominos += dominos
+    return jeu_dominos
