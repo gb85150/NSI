424edad4087b1d4d22edc25bc781e24ce8c2b8de
sf3868 <geoffrey.bousseau@gmail.com>
2022-03-08 12:54:21 +0100

    insane ! ça marche !

--- HomeworkSync/playground.py
+++ HomeworkSync/playground.py
@@ -1,3 +1,5 @@
+from multiprocessing.sharedctypes import Value
+from types import MethodType
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -10,8 +12,8 @@ import time
 
 def connect():
     # Find the username and password fields
-    username = chrome.find_element_by_id("username")
-    password = chrome.find_element_by_id("password")
+    username = chrome.find_element(by=By.ID, value="username")
+    password = chrome.find_element(by=By.ID, value="password")
 
     # Enter the username and password
 
@@ -19,28 +21,37 @@ def connect():
     password.send_keys("01099011109gb")
 
     # Check information provided
-    showpass = chrome.find_element_by_id("show-password")
-    showpass.click()
+    # showpass = chrome.find_element_by_id("show-password")
+    # showpass.click()
 
 
-    login = chrome.find_element_by_id("connexion")
+    login = chrome.find_element(by=By.ID, value="connexion")
     login.click()
 
 
 # Initialize the driver
-chrome = webdriver.Chrome(executable_path=r'F:\NSI\NSI\HomeworkSync\res\chromedriver.exe')
+chrome = webdriver.Chrome(executable_path=r'E:\NSI\NSI\HomeworkSync\res\chromedriver.exe')
 chrome.get("https://www.ecoledirecte.com/")
 
 
 if __name__ == "__main__":
+    print("\n \n \n starting...\n \n \n")	
     connect()
     time.sleep(3)
     homework = chrome.find_element(by=By.XPATH, value='//*[@id="container-menu"]/ed-menu/div/div/div/ul/li[7]/ed-menu-block-item/div/a')
     homework.click()
-    hwcalendar = chrome.find_elements(by=By.CSS_SELECTOR, value=".date")
-    for i in hwcalendar:
-        print(i.text)
-        i.click()
-        time.sleep(3)
-        chrome.back()
-        time.sleep(3)
+    time.sleep(1.5)
+    hwcalendar = chrome.find_elements(by=By.CLASS_NAME, value="date")
+    for i in range(len(hwcalendar)):
+        print(hwcalendar[i].text)
+        hwcalendar[i].click()
+        time.sleep(1)
+        back=chrome.find_element(by=By.XPATH, value='/html/body/app-root/div/ed-authenticated-layout/div[2]/div[2]/ed-cdt/ed-cdt-eleve/div/div/div[2]/div[2]/ed-cdt-eleve-onglets/ul/li[9]/a')
+        back.click()
+        time.sleep(1)
+        hwcalendar = chrome.find_elements(by=By.CLASS_NAME, value="date")
+    # daytab = chrome.find_element(by=By.XPATH, value='/html/body/app-root/div/ed-authenticated-layout/div[2]/div[2]/ed-cdt/ed-cdt-eleve/div/div/div[2]/div[2]/ed-cdt-eleve-onglets/ul/li[2]/a')
+    # daytab.click()
+    print("\n \n \n finished...\n \n \n")
+
+# NB : 0.7s serait peut etre bien pour le sleep
